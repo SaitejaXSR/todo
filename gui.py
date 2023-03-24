0702a3a790dba01dcb9055a6aef1aa34ec29de6c
Sai teja chary <saitejachary1411@gmail.com>
2023-03-24 23:57:34 +0530

    Implement Edit feature

--- gui.py
+++ gui.py
@@ -4,11 +4,11 @@ import PySimpleGUI as gui
 label = gui.Text("Type in a todo")
 input_box = gui.InputText(tooltip="Enter a todo", key='todo')
 add_button = gui.Button("Add")
+list_box = gui.Listbox(values=functions.get_todos(), key='todos',
+                       enable_events=True, size=[45, 10])
+edit_button = gui.Button("Edit")
 
-
-window = gui.Window("To-Do App",
-                    layout=[[label], [input_box], [add_button]],
-                    font=('Mukta', 14))
+window = gui.Window("To-Do App", layout=[[label], [input_box], [add_button, edit_button], [list_box]], font=('Mukta', 14))
 
 while True:
     event, values = window.read()
@@ -22,7 +22,14 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case gui.WIN_CLOSED:
             break
 
